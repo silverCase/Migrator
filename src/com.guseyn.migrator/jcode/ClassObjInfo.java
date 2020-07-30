d18b05a2d5330b8e9584c91cc24eb6d2f2de8a85
guseyn <guseynism@gmail.com>
2020-07-30 18:38:30 +0300

    Restructuring code for parsing java structures

---
+++ src/com.guseyn.migrator/jcode/ClassObjInfo.java
@@ -0,0 +1,12 @@
+package jcode;
+
+public class ClassObjInfo {
+
+	public String className;
+	public String instanceName;
+
+	public ClassObjInfo() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
