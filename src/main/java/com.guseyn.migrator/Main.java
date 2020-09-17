c0031802f97f12e5f195ec402913c575fae649fb
silverCase <serebrennikov.oleg.p@gmail.com>
2020-09-17 05:39:04 +0300

    Updated to work with multiple repos

--- src/main/java/com.guseyn.migrator/Main.java
+++ src/main/java/com.guseyn.migrator/Main.java
@@ -10,22 +10,35 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        String gitRepo = args[0];
-        String outputFile = args[1];
-        Files.createDirectory(Paths.get("clone"));
-        Files.createFile(Paths.get("clone/commits.txt"));
-        CollectingGitCommitsThenFillingUpRepositoriesAndRepoCommitsAndRepoLibrariesInMemoryStorageWithData.main(new String[]{gitRepo});
-        FindingMigrationRules.main(args);
-        // DetectingCodeSegmentsByMigrationRules.main(args);
-        FileUtils.deleteDirectory(new File("clone"));
-        Gson gson = new Gson();
-        BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
-        writer.write(gson.toJson(MemoryStorage.migrationRules));
-        writer.close();
-        System.out.println("done");
+        List<String> gitRepos = FileUtils.readLines(new File(args[0]));
+        for (int i = 0; i < gitRepos.size(); i++) {
+            String[] gitReposSplit = gitRepos.get(i).split("/");
+            String RepoName = gitReposSplit[gitReposSplit.length-1];
+            String outputFile = "out/" + RepoName + ".json";
+            File file = new File("clone/commits.txt");
+            file.createNewFile();
+            CollectingGitCommitsThenFillingUpRepositoriesAndRepoCommitsAndRepoLibrariesInMemoryStorageWithData.main(new String[]{gitRepos.get(i)});
+            //String[] newArgs = new String[] {gitRepos.get(i), outputFile};
+            FindingMigrationRules.main(args);
+            // DetectingCodeSegmentsByMigrationRules.main(args);
+            FileUtils.forceDelete(new File("clone/commits.txt"));
+            Gson gson = new Gson();
+            BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
+            List lst = new ArrayList();
+            lst.add(MemoryStorage.migrationRules);
+            lst.add(MemoryStorage.repoLibraries.get(0).repoLink);
+            writer.write(gson.toJson(lst));
+            writer.close();
+            System.out.println("done");
+        };
     }
 }
